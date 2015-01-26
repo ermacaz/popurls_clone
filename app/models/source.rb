c80d9dad4da68fde4b5d433ac9c98e789fbe553f
MattHamada <mattahamada@gmail.com>
2015-01-26 13:17:00 -0700

    can export sources now to generate rake task

--- app/models/source.rb
+++ app/models/source.rb
@@ -4,4 +4,21 @@ class Source < ActiveRecord::Base
   def get_feed
     rss = SimpleRSS.parse open(feed_url)
   end
+
+  def self.export_as_task
+    output_file = Rails.root.join('sample_data.rake')
+    File.open(output_file, 'w') do |file|
+      file.puts("namespace :db do\n")
+      file.puts("\tdesc 'Fill database with sources'\n")
+      file.puts("\ttask populate: :environment do\n")
+      Source.all.each do |source|
+        file.puts("\t\tSource.create(name: '#{source.name}',\n")
+        file.puts("\t\t\turl: '#{source.url}'\n")
+        file.puts("\t\t\tfeed_url: '#{source.feed_url}')\n\n")
+      end
+      file.puts("\tend\n")
+      file.puts("end\n")
+    end
+
+  end
 end
